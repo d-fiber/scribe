2f073c75ba6b8929df2bdf43e2b52ea9180d5ab7
Larsson <larsanov.inc@gmail.com>
2026-08-31 22:00:14 +0200

    [CHORE]: separate the extension declarations' header from its code

--- engine/runtime/support/extensions/declarations.ts
+++ engine/runtime/support/extensions/declarations.ts
@@ -33,6 +33,7 @@
 //
 // This header is a summary written for convenience. Where it differs from the
 // LICENSE file, the LICENSE file governs.
+
 import type { Future } from "@scribe/alchemy";
 import { isMissingModule } from "./missing_module.ts";
 
